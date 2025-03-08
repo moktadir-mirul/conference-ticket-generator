d5dff7f6740b703db9084e65e5625a985fb9943b
moktadir-mirul <moktadir.mirul@gmail.com>
2025-03-08 17:34:51 +0600

    ticket design complete

--- script/action.js
+++ script/action.js
@@ -1,14 +1,19 @@
 const form = document.querySelector("form");
 
 form.addEventListener("submit", function (event) {
+    event.preventDefault();
   const imageFile = document.getElementById("avatar").files[0];
   const title = document.getElementById("title").value;
   const email = document.getElementById("email").value;
   const ticketTitle = document.getElementById("user-name");
   const ticketMail = document.getElementById("user-mail");
   const ticketImg = document.getElementById("ticket-img");
+  const gitUserName = document.getElementById("git-name");
   const formPage = document.getElementById("form-page");
   const ticketPage = document.getElementById("ticket-page");
+  const ticketPersonName = document.getElementById("ticket-person");
+  const gitName = document.getElementById('ticket-gitname');
+  const onTicketImage = document.getElementById('ticket-img');
   event.preventDefault();
   console.log("hello");
   console.log(imageFile);
@@ -19,11 +24,13 @@ form.addEventListener("submit", function (event) {
   console.log(ticketMail);
   ticketTitle.innerText = title;
   ticketMail.innerText = email;
+  ticketPersonName.innerText = title;
+//   gitName.innerText = gitUserName.value;
   const reader = new FileReader(); // Create a FileReader object
 
   reader.onload = function (e) {
-    ticketImg.src = e.target.result; // Set the <img> src to the file's data URL
-    ticketImg.style.display = "block"; // Make the <img> visible
+    onTicketImage.src = e.target.result; // Set the <img> src to the file's data URL
+    // ticketImg.style.display = "block"; // Make the <img> visible
   };
 
   reader.readAsDataURL(imageFile); // Read the file as a data URL
